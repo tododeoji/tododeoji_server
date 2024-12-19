56d0ae0ae00acde61cf843f759ac81fe81a7d671
huuuunee <s21024@gsm.hs.kr>
2024-12-19 19:34:29 +0900

    Setting

---
+++ src/main/kotlin/com/todo/deoji/DeojiApplication.kt
@@ -0,0 +1,11 @@
+package com.todo.deoji
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeojiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeojiApplication>(*args)
+}
---
+++ src/test/kotlin/com/todo/deoji/DeojiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todo.deoji
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeojiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
