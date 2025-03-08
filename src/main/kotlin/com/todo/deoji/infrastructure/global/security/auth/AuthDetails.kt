30114eb2ab026900a33cd41c07fa9782722ad1eb
huuuunee <s21024@gsm.hs.kr>
2025-03-08 16:01:15 +0900

    AuthDetails에 OAuth2User 구현체 등록

--- src/main/kotlin/com/todo/deoji/infrastructure/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/com/todo/deoji/infrastructure/global/security/auth/AuthDetails.kt
@@ -4,11 +4,22 @@ import com.todo.deoji.core.domain.user.model.User
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.oauth2.core.user.OAuth2User
 
 
 class AuthDetails(
-    private val user: User
-) : UserDetails {
+    private val user: User,
+    private val attribute: Map<String, Any>? = null,
+    private val attributeKey: String? = null
+) : UserDetails, OAuth2User {
+    override fun getName(): String =
+        attribute!![attributeKey].toString()
+
+
+    override fun getAttributes(): MutableMap<String, Any> =
+        mutableMapOf("roles" to user.roles.map { SimpleGrantedAuthority(it.toString()) })
+
+
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
         user.roles
             .map { SimpleGrantedAuthority(it.name) }
