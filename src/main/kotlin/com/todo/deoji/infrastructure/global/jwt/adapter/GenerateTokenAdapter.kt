16f1e6b15a8a54166f9e69c8e8eff18af09aacac
huuuunee <s21024@gsm.hs.kr>
2025-03-08 16:00:18 +0900

    토큰 발급 편하게 리팩토링

--- src/main/kotlin/com/todo/deoji/infrastructure/global/jwt/adapter/GenerateTokenAdapter.kt
+++ src/main/kotlin/com/todo/deoji/infrastructure/global/jwt/adapter/GenerateTokenAdapter.kt
@@ -1,16 +1,19 @@
 package com.todo.deoji.infrastructure.global.jwt.adapter
 
+import com.todo.deoji.core.domain.auth.dto.response.TokenResponseDto
+import com.todo.deoji.core.domain.auth.model.Role
+import com.todo.deoji.core.domain.auth.spi.JwtPort
 import com.todo.deoji.infrastructure.global.jwt.properties.JwtProperty
-import io.jsonwebtoken.Header
 import io.jsonwebtoken.Jwts
 import org.springframework.stereotype.Component
 import java.security.Key
+import java.time.LocalDateTime
 import java.util.*
 
 @Component
 class GenerateTokenAdapter(
     private val jwtProperty: JwtProperty,
-) {
+) : JwtPort {
     object JwtPrefix {
         const val ACCESS = "access"
         const val REFRESH = "refresh"
@@ -18,25 +21,33 @@ class GenerateTokenAdapter(
         const val PREFIX = "Bearer "
     }
 
+    override fun generateToken(userId: String): TokenResponseDto =
+        TokenResponseDto(
+            accessToken = generateAccessToken(userId),
+            refreshToken = generateRefreshToken(userId),
+            accessTokenExp = LocalDateTime.now().withNano(0).plusSeconds(jwtProperty.accessTokenExpiredTime),
+            refreshTokenExp = LocalDateTime.now().withNano(0).plusSeconds(jwtProperty.refreshTokenExpiredTime)
+        )
+
     private fun generateAccessToken(userId: String): String =
         generateToken(jwtProperty.secretKey, JwtPrefix.ACCESS, userId, jwtProperty.accessTokenExpiredTime)
 
     private fun generateRefreshToken(userId: String): String =
-        generateToken(jwtProperty.refreshKey, JwtPrefix.REFRESH, null, jwtProperty.refreshTokenExpiredTime)
-//            .apply {
-//                commandRefreshTokenPort
-//                    .save(RefreshToken(userId, this, jwtProperty.refreshTokenExpiredTime))
-//            }
+        generateToken(jwtProperty.refreshKey, JwtPrefix.REFRESH, userId, jwtProperty.refreshTokenExpiredTime)
 
-    private fun generateToken(secret: Key, jwtType: String, userId: String? = null, expiredTime: Int): String =
-        Jwts.builder()
+    private fun generateToken(secret: Key, jwtType: String, userId: String, expiredTime: Long): String {
+        val now = Date()
+        val expiration = Date(now.time + expiredTime * 1000)
+
+        val authorities = arrayListOf(Role.USER)
+
+        return Jwts.builder()
+            .subject(userId)
+            .claim("type", jwtType)
+            .claim("roles", authorities)
+            .issuedAt(now)
+            .expiration(expiration)
             .signWith(secret)
-            .setHeaderParam(Header.JWT_TYPE, jwtType)
-            .setIssuedAt(Date())
-            .setExpiration(Date(System.currentTimeMillis() + expiredTime * 1000))
-            .apply {
-                if (userId != null)
-                    this.setId(userId)
-            }
             .compact()
+    }
 }
