948cb0e9eb16466d2608e3d7079b05190c1c255a
huuuunee <s21024@gsm.hs.kr>
2025-03-15 17:29:02 +0900

    성공했을때 Status 변경

--- src/main/kotlin/com/todo/deoji/presentation/domain/category/CategoryWebAdapter.kt
+++ src/main/kotlin/com/todo/deoji/presentation/domain/category/CategoryWebAdapter.kt
@@ -4,6 +4,7 @@ import com.todo.deoji.core.domain.category.usecase.AddCategoryUseCase
 import com.todo.deoji.presentation.common.WebAdapter
 import com.todo.deoji.presentation.domain.category.data.extension.toAddCategoryRequestDto
 import com.todo.deoji.presentation.domain.category.data.request.AddCategoryRequestData
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,5 +17,5 @@ class CategoryWebAdapter(
     fun addCategory(@RequestBody addCategoryRequestData: AddCategoryRequestData): ResponseEntity<Void> =
         addCategoryUseCase.execute(
             addCategoryRequestData.toAddCategoryRequestDto()
-        ).run { ResponseEntity.ok().build() }
+        ).run { ResponseEntity.status(HttpStatus.CREATED).build() }
 }
