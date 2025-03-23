f2640832058ba7dcf97f00eb4963577dcfdde138
huuuunee <s21024@gsm.hs.kr>
2025-03-23 16:23:37 +0900

    투두 데이터 조회 함수 생성 및 구현

--- src/main/kotlin/com/todo/deoji/core/domain/todo/spi/QueryTodoPort.kt
+++ src/main/kotlin/com/todo/deoji/core/domain/todo/spi/QueryTodoPort.kt
@@ -1,8 +1,13 @@
 package com.todo.deoji.core.domain.todo.spi
 
 import com.todo.deoji.core.domain.category.model.Category
+import com.todo.deoji.core.domain.todo.dto.response.GetMainDataTodoResponseDto
+import com.todo.deoji.core.domain.todo.model.Todo
+import com.todo.deoji.core.domain.user.model.User
 import java.time.LocalDateTime
 
 interface QueryTodoPort {
     fun findMaxSortByCategoryAndRunDate(category: Category, runDate: LocalDateTime): Int
+    fun findAllByMonthAndYearAndUser(month: Int, year: Int, user: User): List<GetMainDataTodoResponseDto>
+    fun findAllByCategoryIdsAndMonthAndYear(categoryIds: List<Long>, month: Int, year: Int): List<Todo>
 }
