915e370c95b86e9896b56b1d07279560c3dd0179
huuuunee <s21024@gsm.hs.kr>
2025-03-30 02:55:29 +0900

    카테고리 순서 및 숨김 수정 Extension 생성

--- src/main/kotlin/com/todo/deoji/presentation/domain/category/data/extension/CategoryRequestExtension.kt
+++ src/main/kotlin/com/todo/deoji/presentation/domain/category/data/extension/CategoryRequestExtension.kt
@@ -1,7 +1,11 @@
 package com.todo.deoji.presentation.domain.category.data.extension
 
 import com.todo.deoji.core.domain.category.dto.request.AddCategoryRequestDto
+import com.todo.deoji.core.domain.category.dto.request.EditCategoryDetailRequestDto
+import com.todo.deoji.core.domain.category.dto.request.EditCategoryRequestDto
 import com.todo.deoji.presentation.domain.category.data.request.AddCategoryRequestData
+import com.todo.deoji.presentation.domain.category.data.request.EditCategoryDetailRequestData
+import com.todo.deoji.presentation.domain.category.data.request.EditCategoryRequestData
 
 
 fun AddCategoryRequestData.toAddCategoryRequestDto(): AddCategoryRequestDto =
@@ -9,3 +13,20 @@ fun AddCategoryRequestData.toAddCategoryRequestDto(): AddCategoryRequestDto =
         categoryName = this.categoryName,
         colorCode = this.colorCode
     )
+
+fun EditCategoryDetailRequestData.toEditCategoryDetailRequestDto(): EditCategoryDetailRequestDto =
+    EditCategoryDetailRequestDto(
+        categoryId = this.categoryId,
+        categoryName = this.categoryName,
+        categoryColor = this.categoryColor
+    )
+
+fun EditCategoryRequestData.toEditCategoryRequestDtoList(): List<EditCategoryRequestDto> =
+    this.categoryList
+        .map {
+            EditCategoryRequestDto(
+                categoryId = it.categoryId,
+                hideStatus = it.hideStatus,
+                sort = it.sort
+            )
+        }
