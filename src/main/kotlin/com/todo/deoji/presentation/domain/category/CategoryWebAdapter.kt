ecc5a368825d392aa419d873240193f1b2ff0485
huuuunee <s21024@gsm.hs.kr>
2025-03-30 02:56:25 +0900

    카테고리 수정 URL 연결

--- src/main/kotlin/com/todo/deoji/presentation/domain/category/CategoryWebAdapter.kt
+++ src/main/kotlin/com/todo/deoji/presentation/domain/category/CategoryWebAdapter.kt
@@ -1,23 +1,28 @@
 package com.todo.deoji.presentation.domain.category
 
-import com.todo.deoji.core.domain.category.dto.response.GetCategoryListResponseDto
 import com.todo.deoji.core.domain.category.usecase.AddCategoryUseCase
+import com.todo.deoji.core.domain.category.usecase.EditCategoryUseCase
+import com.todo.deoji.core.domain.category.usecase.EditCategoryDetailUseCase
 import com.todo.deoji.core.domain.category.usecase.GetCategoryListUseCase
 import com.todo.deoji.presentation.common.WebAdapter
-import com.todo.deoji.presentation.domain.category.data.extension.toAddCategoryRequestDto
-import com.todo.deoji.presentation.domain.category.data.extension.toData
+import com.todo.deoji.presentation.domain.category.data.extension.*
 import com.todo.deoji.presentation.domain.category.data.request.AddCategoryRequestData
+import com.todo.deoji.presentation.domain.category.data.request.EditCategoryDetailRequestData
+import com.todo.deoji.presentation.domain.category.data.request.EditCategoryRequestData
 import com.todo.deoji.presentation.domain.category.data.response.GetCategoryListResponseData
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 @WebAdapter("/category")
 class CategoryWebAdapter(
     private val addCategoryUseCase: AddCategoryUseCase,
-    private val getCategoryListUseCase: GetCategoryListUseCase
+    private val getCategoryListUseCase: GetCategoryListUseCase,
+    private val editCategoryDetailUseCase: EditCategoryDetailUseCase,
+    private val editCategoryUseCase: EditCategoryUseCase
 ) {
     @PostMapping
     fun addCategory(@RequestBody addCategoryRequestData: AddCategoryRequestData): ResponseEntity<Void> =
@@ -28,4 +33,14 @@ class CategoryWebAdapter(
     @GetMapping("/list")
     fun getUserCategory(): ResponseEntity<List<GetCategoryListResponseData>> =
         ResponseEntity.ok().body(getCategoryListUseCase.execute().map { it.toData() })
+
+    @PatchMapping("/edit-detail")
+    fun editCategory(@RequestBody editCategoryRequestData: EditCategoryDetailRequestData): ResponseEntity<Void> =
+        editCategoryDetailUseCase.execute(editCategoryRequestData.toEditCategoryDetailRequestDto())
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @PatchMapping("/edit")
+    fun editCategorySort(@RequestBody editCategorySortRequestData: EditCategoryRequestData): ResponseEntity<Void> =
+        editCategoryUseCase.execute(editCategorySortRequestData.toEditCategoryRequestDtoList())
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 }
