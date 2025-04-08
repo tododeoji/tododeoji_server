ebddf24e28e8d6a908581622a574c47134d9dd84
huuuunee <s21024@gsm.hs.kr>
2025-04-08 09:53:42 +0900

    투두 수정 Data, Dto, Extension 생성

--- src/main/kotlin/com/todo/deoji/presentation/domain/todo/data/extension/TodoRequestExtension.kt
+++ src/main/kotlin/com/todo/deoji/presentation/domain/todo/data/extension/TodoRequestExtension.kt
@@ -1,7 +1,9 @@
 package com.todo.deoji.presentation.domain.todo.data.extension
 
 import com.todo.deoji.core.domain.todo.dto.request.AddTodoRequestDto
+import com.todo.deoji.core.domain.todo.dto.request.EditTodoRequestDto
 import com.todo.deoji.presentation.domain.todo.data.request.AddTodoRequestData
+import com.todo.deoji.presentation.domain.todo.data.request.EditTodoRequestData
 
 fun AddTodoRequestData.toAddTodoRequestDto(): AddTodoRequestDto =
     AddTodoRequestDto(
@@ -11,3 +13,16 @@ fun AddTodoRequestData.toAddTodoRequestDto(): AddTodoRequestDto =
         endDateTime = this.endDateTime,
         categoryId = this.categoryId
     )
+
+fun EditTodoRequestData.toEditTodoRequestDtoList(): List<EditTodoRequestDto> =
+    this.todoList
+        .map {
+            EditTodoRequestDto(
+                id = it.id,
+                activeStatus = it.activeStatus,
+                todoName = it.todoName,
+                startDateTime = it.startDateTime,
+                endDateTime = it.endDateTime,
+                sort = it.sort
+            )
+        }
