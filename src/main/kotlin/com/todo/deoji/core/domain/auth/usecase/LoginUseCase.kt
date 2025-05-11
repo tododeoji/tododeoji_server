85dcdbf792295a3bc30c17d47d79768831eb8a76
huuuunee <s21024@gsm.hs.kr>
2025-05-11 16:31:31 +0900

    로그인시 리프레시 토큰 저장 로직 추가

--- src/main/kotlin/com/todo/deoji/core/domain/auth/usecase/LoginUseCase.kt
+++ src/main/kotlin/com/todo/deoji/core/domain/auth/usecase/LoginUseCase.kt
@@ -2,12 +2,25 @@ package com.todo.deoji.core.domain.auth.usecase
 
 import com.todo.deoji.core.common.annotation.UseCase
 import com.todo.deoji.core.domain.auth.dto.response.TokenResponseDto
+import com.todo.deoji.core.domain.auth.model.RefreshToken
 import com.todo.deoji.core.domain.auth.spi.JwtPort
+import com.todo.deoji.core.domain.auth.spi.RefreshTokenPort
 
 @UseCase
 class LoginUseCase(
-    private val jwtPort: JwtPort
+    private val jwtPort: JwtPort,
+    private val refreshTokenPort: RefreshTokenPort
 ) {
-    fun execute(userId: String): TokenResponseDto =
-        jwtPort.generateToken(userId)
+    fun execute(userId: String): TokenResponseDto {
+        val tokenResponseDto = jwtPort.generateToken(userId)
+
+        refreshTokenPort.saveRefreshToken(
+            RefreshToken(
+                refreshToken = tokenResponseDto.refreshToken,
+                userId = userId
+            )
+        )
+
+        return tokenResponseDto
+    }
 }
